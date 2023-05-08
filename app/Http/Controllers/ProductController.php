83dceddb95319a53f72d07da6425c70f03ccfbb5
Marco Lorenzo Oliva <marco.lorenzo@iesdonana.org>
2023-05-08 22:29:48 +0200

    Antes de liarla con Vite

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,14 +3,113 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
+
+use App\Http\Requests\Product\StoreRequest;
+use App\Http\Requests\Product\UpdateRequest;
 
 class ProductController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function productList()
     {
         $products = Product::all();
 
         return view('products', compact('products'));
+    }
+
+    public function index(): Response
+    {
+        return response()->view('products', [
+            'products' => Product::orderBy('updated_at', 'desc')->get(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): Response
+    {
+        return response()->view('products.form');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        // insert only requests that already validated in the StoreRequest
+        $create = Product::create($validated);
+
+        if($create) {
+            // add flash for the success notification
+            session()->flash('notif.success', 'Post created successfully!');
+            return redirect()->route('products');
+        }
+
+        return abort(500);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): Response
+    {
+        return response()->view('products.show', [
+            'products' => Product::findOrFail($id),
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id): Response
+    {
+        return response()->view('products.form', [
+            'products' => Product::findOrFail($id),
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): RedirectResponse
+    {
+        $product = Product::findOrFail($id);
+        $validated = $request->validated();
+
+        $update = $product->update($validated);
+
+        if($update) {
+            session()->flash('notif.success', 'Post updated successfully!');
+            return redirect()->route('products');
+        }
+
+        return abort(500);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): RedirectResponse
+    {
+        $product = Product::findOrFail($id);
+
+        $delete = $product->delete($id);
+
+        if($delete) {
+            session()->flash('notif.success', 'Post deleted successfully!');
+            return redirect()->route('products');
+        }
+
+        return abort(500);
     }
 }
