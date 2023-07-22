91d4c44a4c91ab4d4cdef370b23d36a1afab2762
Marco Lorenzo <marco.lorenzo@iesdonana.org>
2023-07-23 01:31:21 +0200

    cambios a botones de idioma

--- resources/views/partials/language_switcher.blade.php
+++ resources/views/partials/language_switcher.blade.php
@@ -1,10 +1,10 @@
 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0" style="display:flex; gap:30px;">
     @foreach($available_locales as $locale_name => $available_locale)
         @if($available_locale === $current_locale)
-            <span class="ml-2 mr-2 text-gray-700">{{ $locale_name }}</span>
+            <span class="ml-2 mr-2 text-gray-700" style="background:green; color:white; padding:10px; border-radius:30px;">{{ $locale_name }}</span>
         @else
-            <a class="ml-1 underline ml-2 mr-2" href="language/{{ $available_locale }}">
-                <span>{{ $locale_name }}</span>
+            <a class="ml-1 ml-2 mr-2" href="language/{{ $available_locale }}">
+                <span style="background:red; color:white; padding:10px; border-radius:30px;">{{ $locale_name }}</span>
             </a>
         @endif
     @endforeach
