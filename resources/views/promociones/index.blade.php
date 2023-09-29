942a4de161a75101840f82a895064053ac39f3a0
Marco Lorenzo Oliva <marco.lorenzo@iesdonana.org>
2023-09-29 14:14:12 +0200

    efectos promociones

--- resources/views/promociones/index.blade.php
+++ resources/views/promociones/index.blade.php
@@ -8,29 +8,31 @@
             @include('partials/language_switcher')
         </div>
     </x-slot>
+    <link rel="stylesheet" href="/css/promociones.css" />
     <br>
     <div class="bg-white" style="text-align:center;">
         <br>
-        <img src="img/pizzacoin.png" alt="coin" style="margin-left:auto; margin-right:auto;" width="100px" height="100px">
+        <img src="img/pizzacoin.png" alt="coin" style="margin-left:auto; margin-right:auto;" width="100px"
+            height="100px">
         <br>
-        {{__('¿QUÉ SON LAS PIZZACOINS?')}}
+        {{ __('¿QUÉ SON LAS PIZZACOINS?') }}
     </div>
     <div class="bg-white" style="text-align:center;">
         <br>
-        {{__('Las pizzacoins son la moneda exclusiva de la Pizzería Brenda.')}}
+        {{ __('Las pizzacoins son la moneda exclusiva de la Pizzería Brenda.') }}
         <br>
-        {{__('Puedes usar estas monedas para canjearlas por promociones especiales.')}}
+        {{ __('Puedes usar estas monedas para canjearlas por promociones especiales.') }}
         <br>
-        {{__('Cada vez que realices un pedido de cualquier menú o producto en la página web, obtendrás Pizzacoins. Por cada céntimo que gastes, recibirás 1 Pizzacoin.')}}
+        {{ __('Cada vez que realices un pedido de cualquier menú o producto en la página web, obtendrás Pizzacoins. Por cada céntimo que gastes, recibirás 1 Pizzacoin.') }}
         <br>
-        {{__('¡Acumula esas Pizzacoins y píllate un menú gratis!')}}
+        {{ __('¡Acumula esas Pizzacoins y píllate un menú gratis!') }}
     </div>
     <br>
     <div class="bg-white" style="text-align:center;">
-        {{__('ACUMULA PIZZACOINS PARA CANJEARLAS POR PROMOCIONES')}}
+        {{ __('ACUMULA PIZZACOINS PARA CANJEARLAS POR PROMOCIONES') }}
     </div>
     <div class="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 bg-white"
-    style="flex-wrap:wrap; align-items:center; text-align:center; padding:30px;">
+        style="flex-wrap:wrap; align-items:center; text-align:center; padding:30px;">
         @foreach ($promotions as $promotion)
             @if ($promotion->habilitado and $promotion->type == 'Promoción')
                 <form action="{{ route('cart.store') }}" method="POST" enctype="multipart/form-data">
@@ -47,7 +49,7 @@
                             <input type="image" name="submit" src="{{ asset($promotion->image) }}" alt="submit"
                                 class="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-md shadow-md"
                                 width="422" height="600"
-                                style="border-color:black; border-style:solid; border-width:5px; border-radius:30px;">
+                                style="border-color:black; border-style:solid; border-width:5px; border-radius:30px;" id="imgproducto">
                         @else
                             <div style="background: rgba(0, 0, 0, 0.5);">
                                 <img src="{{ asset($promotion->image) }}" alt="submit"
@@ -62,8 +64,8 @@
                         <div style="display:flex; flex-wrap:wrap;">
                             @if ($promotion->alergenos != '')
                                 @foreach ($alergenoslista as $alergeno)
-                                    <img src="{{ asset('img/alergenos/single/' . $alergeno . '.png') }}"
-                                        width="40px" height="40px">
+                                    <img src="{{ asset('img/alergenos/single/' . $alergeno . '.png') }}" width="40px"
+                                        height="40px">
                                 @endforeach
                             @endif
                         </div>
@@ -71,7 +73,8 @@
                             <div class="text-center" style="font-size:20px;">{{ __('Pizzacoins necesarias: ') }}
                                 {{ $promotion->puntos }}</div>
                         @else
-                            <div class="text-center" style="font-size:20px;">{{ __('Pizzacoins necesarias: ') }} 0</div>
+                            <div class="text-center" style="font-size:20px;">{{ __('Pizzacoins necesarias: ') }} 0
+                            </div>
                         @endif
                         <br><br>
                     </div>
@@ -81,10 +84,10 @@
     </div>
     <br>
     <div class="bg-white" style="text-align:center;">
-        {{__('OFERTAS')}}
+        {{ __('OFERTAS') }}
     </div>
     <div class="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 bg-white"
-    style="flex-wrap:wrap; align-items:center; text-align:center; padding:30px;">
+        style="flex-wrap:wrap; align-items:center; text-align:center; padding:30px;">
         @foreach ($promotions as $promotion)
             @if ($promotion->habilitado and $promotion->type == 'Oferta')
                 <form action="{{ route('cart.store') }}" method="POST" enctype="multipart/form-data">
@@ -101,12 +104,12 @@
                             <input type="image" name="submit" src="{{ asset($promotion->image) }}" alt="submit"
                                 class="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-md shadow-md"
                                 width="422" height="600"
-                                style="border-color:black; border-style:solid; border-width:5px; border-radius:30px;">
+                                style="border-color:black; border-style:solid; border-width:5px; border-radius:30px;" id="imgproducto">
                         @else
                             <div style="background: rgba(0, 0, 0, 0.5);">
                                 <img src="{{ asset($promotion->image) }}" alt="submit"
-                                    class="w-full max-w-sm mx-auto overflow-hidden rounded-md shadow-md" width="422"
-                                    height="600"
+                                    class="w-full max-w-sm mx-auto overflow-hidden rounded-md shadow-md"
+                                    width="422" height="600"
                                     style="border-color:black; border-style:solid; border-width:5px; border-radius:30px;">
                             </div>
                         @endif
@@ -141,7 +144,8 @@
         </span>
         <ul class="hidden flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0 sm:flex">
             <li>
-                <a href="{{ route('whoarewe') }}" class="mr-4 hover:underline md:mr-6">{{ __('¿Quiénes somos?') }}</a>
+                <a href="{{ route('whoarewe') }}"
+                    class="mr-4 hover:underline md:mr-6">{{ __('¿Quiénes somos?') }}</a>
             </li>
             <li>
                 <a href="{{ route('faq') }}"
