0a9f8e8f378e091dc10297d0c4a29f10f30d8c2f
Marco Lorenzo Oliva <marco.lorenzo@iesdonana.org>
2023-10-17 17:42:28 +0200

    intentando arreglar footer

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -12,9 +12,6 @@ use App\Http\Controllers\ProductController;
             {{ __('NUESTRO MENÚ') }}
         </h2>
         <br><br>
-        <div>
-            @include('partials/language_switcher')
-        </div>
         {{--
             <br>
             <a href="{{ route('products.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded-md">AÑADIR PRODUCTO</a>
@@ -1105,9 +1102,14 @@ use App\Http\Controllers\ProductController;
     <footer
         class="fixed bottom-0 left-0 z-20 w-full p-4 border-t border-gray-300 shadow md:flex md:items-center md:justify-between md:p-6"
         style="background-color:red;">
-        <span
-            class="text-sm sm:text-center" style="color: white;">{{ __('© 2023 Pizzería Brenda™. Todos los derechos reservados.') }}
-        </span>
+        <div>
+            @include('partials/language_switcher')
+        </div>
+        {{--
+            <span
+                class="text-sm sm:text-center" style="color: white;">{{ __('© 2023 Pizzería Brenda™. Todos los derechos reservados.') }}
+            </span>
+        --}}
         <ul class="hidden flex-wrap items-center mt-3 text-sm font-medium sm:mt-0 sm:flex" style="color: white;">
             <li>
                 <a href="{{ route('whoarewe') }}"
