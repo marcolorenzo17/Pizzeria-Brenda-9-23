ae27ecba37bdcba46e80dbf5b40d286e32d9261a
Marco Lorenzo Oliva <marco.lorenzo@iesdonana.org>
2023-10-19 14:19:57 +0200

    idiomas en dropdown

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -110,6 +110,11 @@
                     </x-slot>
 
                     <x-slot name="content">
+                        <br>
+                        <div>
+                            @include('partials/language_switcher')
+                        </div>
+                        <br>
                         @if (!Auth::user()->admin)
                             <x-dropdown-link id="productos-pequenio"
                                 onclick="alert('{{ __('| Español |\n\n¿Qué son las Pizzacoins?\n\nLas pizzacoins son la moneda exclusiva de la Pizzería Brenda.\nPuedes usar estas monedas para canjearlas por promociones especiales.\nCada vez que realices un pedido de cualquier menú o producto en la página web, obtendrás Pizzacoins. Por cada céntimo que gastes, recibirás 1 Pizzacoin.\n¡Acumula esas Pizzacoins y píllate un menú gratis!\n\n| English |\n\nWhat are Pizzacoins?\n\nPizzacoins are the exclusive currency of Pizzería Brenda.\nYou can use these coins to exchange them for special promotions.\nEach time you make an order of any menu or product on this website, you will get Pizzacoins. For each cent you spend, you will receive 1 Pizzacoin.\nGather some Pizzacoins and get yourself a free menu!') }}')">
@@ -151,6 +156,7 @@
                                 {{ __('Cerrar sesión') }}
                             </x-dropdown-link>
                         </form>
+
                     </x-slot>
                 </x-dropdown>
             </div>
