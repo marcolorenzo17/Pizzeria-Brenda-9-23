25ef963deabb7da5e1bcb57182da6ac47bcf3a01
Marco Lorenzo Oliva <marco.lorenzo@iesdonana.org>
2023-12-26 19:39:45 +0100

    footer actualizado en todas las paginas

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -194,42 +194,30 @@
     </div>
 
     <br><br><br><br>
-    <footer class="fixed bottom-0 left-0 z-20 w-full p-4 border-t border-gray-300 shadow md:p-6"
-        style="background-color:red;">
-        <span class="text-sm sm:text-center"
-            style="color: white; margin-right:20px;">{{ __('© 2023 Pizzería Brenda™. Todos los derechos reservados.') }}
-        </span>
-        <div class="md:flex md:items-center md:justify-between flex-wrap">
-            <div style="display:flex; gap: 5px; color:white;">
-                <p style="font-size:13px;">{{ __('Teléfonos: ') }}</p>
-                <div>
-                    <p style="font-size:18px; font-weight:bolder;">956 37 11 15</p>
-                    <p style="font-size:18px; font-weight:bolder;">956 37 47 36</p>
-                    <p style="font-size:18px; font-weight:bolder;">627 650 605</p>
+    <div class="footer">
+        <div style="text-align:center;">
+            <p>{{ __('© 2023 Pizzería Brenda™. Todos los derechos reservados.') }}</p>
+        </div>
+        <div style="display:flex; flex-wrap:wrap; justify-content:center; align-items:center;">
+            <div style="display:flex; gap: 5px; align-items:center;">
+                <p style="font-size:22px; color:#568c2c; font-weight:bolder; text-transform:uppercase;">
+                    {{ __('Teléfonos: ') }}
+                </p>
+                <div style="font-size:18px; font-weight:bolder;">
+                    <p>956 37 11 15 | 956 37 47 36 | 627 650 605</p>
                 </div>
             </div>
-            <ul class="hidden flex-wrap items-center mt-3 text-sm font-medium sm:mt-0 sm:flex"
-                style="color: white; justify-content:center; margin-left:auto;">
-                <li>
-                    <a href="{{ route('whoarewe') }}"
-                        class="mr-4 hover:underline md:mr-6">{{ __('¿Quiénes somos?') }}</a>
-                </li>
-                <li>
-                    <a href="{{ route('faq') }}"
-                        class="mr-4 hover:underline md:mr-6">{{ __('Preguntas frecuentes') }}</a>
-                </li>
-                <li>
-                    <a href="{{ route('contact') }}" class="mr-4 hover:underline md:mr-6">{{ __('Contáctanos') }}</a>
-                </li>
-                <li>
-                    <a href="{{ route('privacy') }}"
-                        class="mr-4 hover:underline md:mr-6">{{ __('Política de privacidad') }}</a>
-                </li>
-                <li>
-                    <a href="{{ route('premios') }}" class="mr-4 hover:underline md:mr-6">{{ __('Premios') }}</a>
-                </li>
-            </ul>
-            <div style="margin-left:auto; display:flex; justify-content:center;">
+            <div style="margin-left:auto; display:flex; gap:30px; text-align:center;">
+                <a class="anavbar" href="{{ route('whoarewe') }}"
+                    style="font-size:13px;">{{ __('¿Quiénes somos?') }}</a>
+                <a class="anavbar" href="{{ route('faq') }}"
+                    style="font-size:13px;">{{ __('Preguntas frecuentes') }}</a>
+                <a class="anavbar" href="{{ route('contact') }}" style="font-size:13px;">{{ __('Contáctanos') }}</a>
+                <a class="anavbar" href="{{ route('privacy') }}"
+                    style="font-size:13px;">{{ __('Política de privacidad') }}</a>
+                <a class="anavbar" href="{{ route('premios') }}" style="font-size:13px;">{{ __('Premios') }}</a>
+            </div>
+            <div style="margin-left:auto; display:flex;">
                 <a href="https://twitter.com/BRENDAPIZZA" target="__blank"><img src="{{ asset('img/twit.png') }}"
                         width="30px" height="30px" style="margin-right:20px;"></a>
                 <a href="https://www.instagram.com/pizzeriabrenda/?hl=es" target="__blank"><img
@@ -239,16 +227,33 @@
                 <a href="https://www.facebook.com/pizzeriabrenda/?locale=es_ES" target="__blank"><img
                         src="{{ asset('img/face.png') }}" width="30px" height="30px" style="margin-right:20px;"></a>
             </div>
-            <div class="flex-wrap" style="display:flex; gap: 5px; margin-left:auto; color:white;">
-                <p style="font-size:13px;">{{ __('Horario: ') }}</p>
-                <div>
-                    <p style="font-size:18px; font-weight:bolder;">{{ __('De lunes a domingo: 20:30 - 23:30') }}
-                    </p>
-                    <p style="font-size:18px; font-weight:bolder;">{{ __('Domingo por la mañana: 13:30 - 15:00') }}
-                    </p>
+            <div style="display:flex; gap: 5px; margin-left:auto; align-items:center;">
+                <p style="font-size:22px; color:#568c2c; font-weight:bolder; text-transform:uppercase;">
+                    {{ __('Horario: ') }}
+                </p>
+                <div style="font-size:18px; font-weight:bolder;">
+                    <p>{{ __('De lunes a domingo: 20:30 - 23:30') }}</p>
+                    <p>{{ __('Domingo por la mañana: 13:30 - 15:00') }}</p>
                 </div>
             </div>
         </div>
-    </footer>
+    </div>
+
+    <style>
+        .footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #141414;
+            color: white;
+            padding: 20px;
+            z-index: 1;
+        }
+
+        .anavbar:hover {
+            text-decoration: underline;
+        }
+    </style>
 
 </x-app-layout>
